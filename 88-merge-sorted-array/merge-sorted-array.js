be43d662c5c4e1052b913145c7c30c0ab103384b
Hojoon Kim <124651600+hojoon0724@users.noreply.github.com>
2024-06-09 16:42:23 -0700

    Time: 60 ms (21.31%) | Memory: 49.2 MB (28.99%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -0,0 +1,21 @@
+// nums1 + nums2 ordered array
+// m === nums1.length, n === nums2.length
+
+// output: nums1 === nums1 + nums2 merged
+// 
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+var merge = function (nums1, m, nums2, n) {
+    nums1.length = m;
+    for (let i = 0; i < n; i++) {
+        nums1.push(nums2[i]);
+    }
+    nums1.sort((a, b) => a - b);
+};
