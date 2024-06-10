31c151cb99aebbd6f164d5e2dc5906142b55e083
Hojoon Kim <124651600+hojoon0724@users.noreply.github.com>
2024-06-09 21:56:35 -0700

    Time: 63 ms (46.89%) | Memory: 52.8 MB (33.90%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,29 +3,17 @@
  * @return {number}
  */
 function majorityElement(nums) {
-  let maxCount = 1;
-  let value = 0;
-  let innerCount = 1;
-
-  if (nums.length === 1) {
-    return nums[0];
-  } else {
-    nums.sort((a, b) => a - b);
-    console.log(`ordered ${nums}`);
-    for (i = 0; i < nums.length; i++) {
-      if (nums[i] === nums[i + 1]) {
-        innerCount = innerCount + 1;
-        console.log(`index[${i}]  ${nums[i]}:${nums[i + 1]} -- repeat -- count: ${innerCount} `);
-      } else {
-        if (innerCount > maxCount) {
-          maxCount = innerCount;
-          value = nums[i];
+    let cand = null;
+    let count = 0;
+    for (num of nums) {
+        if (count == 0) {
+            cand = num;
         }
-        innerCount = 1;
-        console.log(`index[${i}]  ${nums[i]}:${nums[i + 1]} -- reset`);
-      }
+        if (num == cand) {
+            count++;
+        } else {
+            count--;
+        }
     }
-  }
-  console.log([maxCount, value]);
-  return value;
+    return cand;
 }
