91a006daff10fec75073f4967342e7577c05a406
Hojoon Kim <124651600+hojoon0724@users.noreply.github.com>
2024-06-09 21:14:02 -0700

    Time: 57 ms (78.06%) | Memory: 51.3 MB (68.17%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.js
@@ -4,25 +4,16 @@
  */
 
 function removeDuplicates(nums) {
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] === nums[i + 2]) {
-      // console.log(`${i} complicated`);
-      j = i + 2;
-      while (nums[i] === nums[j]) {
-        j++;
-      }
-      console.log(`${i} remove from index[${i + 2}], ${j - 1 - (i + 1)}`);
-      console.log(`i=${i}/j=${j}`);
-      nums.splice(i + 2, j - 1 - (i + 1));
-      console.log(nums);
-      i++;
-    } else if (nums[i] === nums[i + 1]) {
-      console.log(`${i} same`);
-      i++;
-    } else if (nums[i] !== nums[i + 1]) {
-      console.log(`${i} diff`);
-      i++;
+  let repeat = false
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) {
+            if (!repeat) repeat = true
+            else {
+                nums.splice(i, 1)
+                i--
+            }
+        }
+        else repeat = false
     }
-  }
+    return nums.length
 }
