e5b97ba309faa1ddc887ab43526666dd6c6ae4a0
Hojoon Kim <124651600+hojoon0724@users.noreply.github.com>
2024-06-10 20:08:19 -0700

    Time: 130 ms (65.12%) | Memory: 58.4 MB (35.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -6,18 +6,8 @@
 let num = 0
 
 function isPalindrome(x) {
-    num = x.toString()
-    let arr = num.split('')
-    console.log(arr)
-    let i = 0
-    let z = arr.length - 1
-    while (i <= z) {
-        if (arr[i] === arr[z]) {
-            i++;
-            z--;
-        } else {
-            return false
-        }
+    if ( String(x) === String(x).split("").reverse().join("")) {
+        return true;
     }
-    return true
+    return false;
 };
