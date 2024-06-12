0a9652c31a39a06863e591a3837899614bc279dc
Hojoon Kim <124651600+hojoon0724@users.noreply.github.com>
2024-06-11 22:47:18 -0700

    Time: 64 ms (8.58%) | Memory: 49.6 MB (10.70%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+function sortColors(arr) {
+  let lo = 0;
+  let mid = 0;
+  let hi = arr.length - 1;
+
+  while (mid <= hi) {
+    if (arr[mid] === 0) {
+      [arr[mid], arr[lo]] = [arr[lo], arr[mid]];
+      lo++;
+      mid++;
+    } else if (arr[mid] === 1) {
+      mid++;
+    } else if (arr[mid] === 2) {
+      [arr[hi], arr[mid]] = [arr[mid], arr[hi]];
+      hi--;
+    }
+  }
+  return arr;
+}
